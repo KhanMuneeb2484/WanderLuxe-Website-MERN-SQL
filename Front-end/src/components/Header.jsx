391f7cb7a72fb112a6aadc774ea8f69120eff09e
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-11-29 19:23:38 +0500

    shuja chakari

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -53,9 +53,6 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <div className="navbar-nav ms-auto py-0">
-              <Link to="/" className="nav-item nav-link text-white">
-                Home
-              </Link>
               <Link to="/About" className="nav-item nav-link text-white">
                 About
               </Link>
