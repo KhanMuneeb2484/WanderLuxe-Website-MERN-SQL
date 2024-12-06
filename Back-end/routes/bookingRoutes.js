7c52880b433d1d42fece6d9dc10a707ed6ced61c
Muneeb Maqsood <muneebmaqsoodkhan@gmail.com>
2024-12-06 10:46:47 +0500

    addin bookings 2

---
+++ Back-end/routes/bookingRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createBooking,
+  deleteBooking,
+} from "../controllers/bookingController.js";
+import { authenticateJWT, requireRole } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+// Route to create a booking
+router.post("/create-booking", authenticateJWT, createBooking);
+
+// Delete booking by ID
+router.delete(
+  "/delete-booking/:booking_id",
+  authenticateJWT,
+  requireRole(["admin"]),
+  deleteBooking
+);
+
+export default router;
